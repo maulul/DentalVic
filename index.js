f8b5e0db64af996e7150760da9da3a8a72c94074
maulul <maulul@gmail.com>
2021-05-17 12:34:21 -0300

    Obtencion de datos del form por DOM

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+//Guardadoi local
+sessionStorage.setItem('proyecto','Lulkin')
+
 //Array de los datos de los pacientes
 const datosPAcientes = []
+
+
 //Mensaje de bbienvenida a la web
 alert ( 'Bienvenido a DentalVic, por favor, ingrese los datos del paciente')
 
 //Funcion que pide los datos del paciente
-function pedirDatos () {
+const pedirDatos = () => {
+
     let idCount = datosPAcientes.length;
-    let nombrePAciente = prompt ('Ingrese su nombre:');
-    let apellidoPaciente = prompt ('Ingrese su apellido:')
-    datosPAcientes.push ({id:idCount,Nombre:nombrePAciente,Apellido:apellidoPaciente});
+    let nombrePAciente = document.getElementById("name").value;
+    let apellidoPaciente = document.getElementById("lastName").value;
+    let numeroDni = document.getElementById("DNI").value;
+    datosPAcientes.push ({id:idCount,Nombre:nombrePAciente,Apellido:apellidoPaciente,DNI:numeroDni});
      
      
+
 }
 
+//Seleccion de consultorio
+function consultorio() {
+    var x = document.getElementById("consSelect").value;
+    document.getElementById("demo").innerHTML = "Elegiste el consultorio: " + x;
+}
 //Loop de agregado de datos de paciente o continuar en la pagina
-do {   
+/*do {   
         pedirDatos()
-        var continueQuest = prompt  ('Desea agregar otro paciente? si/no')
+        let continueQuest = prompt  ('Desea agregar otro paciente? si/no')
 
         if (continueQuest == 'si'){            
             
         } else (alert ( 'Ingreso exitoso'))
 
-}while (continueQuest == 'si');
+}while (continueQuest == 'si');*/
+
+
     
 
