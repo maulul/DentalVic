7ca6c3e330076aa95660c4f7848154c7a199c41d
maulul <maulul@gmail.com>
2021-05-29 19:42:27 -0300

    puliendo las cards

--- index.js
+++ index.js
@@ -1,63 +1,31 @@
 //Array de los datos de los pacientes
 const datosPAcientes = []
 
-const VCJson = [
 
+const VCJson = [{
+        "img":"./imagenes/consul.jpeg",
+        "title": "Villa crespo",
+        "descrip": "Consultorio ubicado en Villa crespo sobre av. Corrientes",
+        "boton": "Sacar turno"
+    },
     {
-        "title": "Villa crespo"
-    ,
-    
-        "descrip": "Consultorio ubicado en Villa crespo sobre av. Corrientes"
-    ,
-    
+        "img":"./imagenes/consul2.jpeg",
+        "title": "Lanus",
+        "descrip": "Consultorio ubicado en Lanus sobre calle esquiu",
         "boton": "Sacar turno"
     }
-
 ]
 
 VCJson.forEach(e => {
-    const midiv = document.querySelector(".VCBody")
+    const midiv = document.querySelector(".items")
     const div = document.createElement("div")
-
+    div.classList.add("cardStyle")
     div.innerHTML =
-        `                              
-    <h5 class="card-title">${e.title}</h5>                                     
-    <p class="card-text">${e.descrip}</p>    
-    <a href="./turnos.html" class="btn btn-primary">${e.boton}</a>
+        `   
+        <img src=${e.img} alt="" width="500" height="300">
+        <h5 class="card-title">${e.title}</h5>                                     
+        <p class="card-text">${e.descrip}</p>    
+        <a href="./turnos.html" class="btn btn-primary">${e.boton}</a>                           
     `;
-
     midiv.appendChild(div)
-
-
 })
-
-const LJson = [
-
-    {
-        "titleL": "Lanus"
-    ,
-    
-        "descripL": "Consultorio ubicado en Lanus sobre calle esquiu"
-    ,
-    
-        "botonL": "Sacar turno"
-    }
-
-]
-
-LJson.forEach(e => {
-    const midiv = document.querySelector(".LBody")
-    const div = document.createElement("div")
-
-    div.innerHTML =
-        `                              
-    <h5 class="card-title">${e.titleL}</h5>                                     
-    <p class="card-text">${e.descripL}</p>    
-    <a href="./turnos.html" class="btn btn-primary">${e.botonL}</a>
-    `;
-
-    midiv.appendChild(div)
-
-
-})
-
