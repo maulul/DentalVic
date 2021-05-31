ecd63caf8ff04c8c7927a7dd1752dc0dfa59760e
maulul <maulul@gmail.com>
2021-05-31 15:04:50 -0300

    arregl ode customs

---
+++ Customs/cardsInfo.js/cards.js
@@ -0,0 +1,27 @@
+const VCJson = [{
+    "img":"./imagenes/consul.jpeg",
+    "title": "Villa crespo",
+    "descrip": "Consultorio ubicado en Villa crespo sobre av. Corrientes",
+    "boton": "Sacar turno"
+},
+{
+    "img":"./imagenes/consul2.jpeg",
+    "title": "Lanus",
+    "descrip": "Consultorio ubicado en Lanus sobre calle esquiu",
+    "boton": "Sacar turno"
+}
+]
+
+VCJson.forEach(e => {
+const midiv = document.querySelector(".items")
+const div = document.createElement("div")
+div.classList.add("cardStyle")
+div.innerHTML =
+    `   
+    <img src=${e.img} alt="" width="500" height="300">
+    <h5 class="card-title">${e.title}</h5>                                     
+    <p class="card-text">${e.descrip}</p>    
+    <a href="./turnos.html" class="btn btn-primary">${e.boton}</a>                           
+`;
+midiv.appendChild(div)
+})
--- Functions/functions.js
+++ Functions/functions.js
@@ -9,17 +9,19 @@ function openNav() {
     document.getElementById("main").style.marginLeft= "0";
   }
 
-
-//Funcion que pide los datos del paciente
+  function captDay(event) {
+    console.log(event.target.value)
+  }
+ 
+//Funcion que guarda los datos del paciente
 const pedirDatos = () => {
 
-    let idCount = datosPAcientes.length;
+    let idCount = datosPacientes.length;
     let nombrePAciente = document.getElementById("name").value;
     let apellidoPaciente = document.getElementById("lastName").value;
     let numeroDni = document.getElementById("DNI").value;
-    let consultorioElegido = document.getElementById("consSelect").value;
-    let diaElegido = document.getElementsByClassName("form-check-label").value;
-    datosPAcientes.push ({id:idCount,Nombre:nombrePAciente,Apellido:apellidoPaciente,DNI:numeroDni,Consultorio:consultorioElegido,Dia:diaElegido});
+    let consultorioElegido = document.getElementById("consSelect").value;    
+    datosPacientes.push ({id:idCount,Nombre:nombrePAciente,Apellido:apellidoPaciente,DNI:numeroDni,Consultorio:consultorioElegido,Dia:diaElegido});
           
 
 }
@@ -46,21 +48,21 @@ function mostrarDias(){
 
 
 //Funcion para mostrar los horarios
-/*function mostrarHorarios(){
-    var var1 = document.getElementById('eleccionDia').value;
-    var var2 = document.getElementById('');   
-    var var3 = document.getElementById('');
-    if(var1=="Villa Crespo"){
-        var2.style.display="block";
+function mostrarHorarios(){
+    var var4 = document.getElementsByClassName('eleccionDia').value;
+    var var5 = document.getElementById('vcHours');   
+    var var6 = document.getElementById('lHours');
+    if(var4== "Martes"){
+        var5.style.display="block";
     }
     else{
-        var2.style.display="none";
+        var5.style.display="none";
     }
 
-    if(var1=="Lanus"){
-        var3.style.display="block";
+    if(var4== "Lunes"){
+        var6.style.display="block";
     }
     else{
-        var3.style.display="none";
+        var6.style.display="none";
     }
-}*/
+}
